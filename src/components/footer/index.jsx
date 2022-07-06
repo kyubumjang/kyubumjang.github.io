0a0ca5a9f2ec66d0303992382cee7e35fd338774
Kyubum Jang <jkb2221@gmail.com>
2022-07-07 00:34:42 +0900

    feat: Add Footer Component

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const footer = () => {
+  return (
+    <footer>
+      © {new Date().getFullYear()}, Built with{' '}
+      <a href='https://www.gatsbyjs.com'>Gatsby</a>
+    </footer>
+  );
+};
+
+export default footer;
